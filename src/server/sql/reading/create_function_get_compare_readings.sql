85979ea0904a74b14d6212240591d9f83b9a91d2
Tyler Thompson <tdthompson650@gmail.com>
2025-07-03 20:32:24 -0700

    Created new function meter_compare_readings_unit_v2 that uses the meter_hourly_readings_unit view

--- src/server/sql/reading/create_function_get_compare_readings.sql
+++ src/server/sql/reading/create_function_get_compare_readings.sql
@@ -105,6 +105,60 @@ END;
 $$ LANGUAGE 'plpgsql';
 
 
+-- New version of the meter_compare_readings function that uses the meter_hourly_readings_unit view.
+CREATE OR REPLACE FUNCTION meter_compare_readings_unit_v2 (
+	meter_ids INTEGER[],
+	g_unit_id INTEGER, 	-- This is the graphic unit id, changed from graphic_unit_id to avoid confusion with the graphic unit id in the view.
+	curr_start TIMESTAMP,
+	curr_end TIMESTAMP,
+	shift INTERVAL
+)
+	RETURNS TABLE(meter_id INTEGER, curr_use FLOAT, prev_use FLOAT)
+AS $$
+DECLARE
+	curr_tsrange TSRANGE;
+	prev_tsrange TSRANGE;
+BEGIN
+	curr_tsrange := tsrange(curr_start, curr_end);
+	prev_tsrange := tsrange(curr_start - shift, curr_end - shift);
+
+	RETURN QUERY
+	WITH
+	curr_period AS (
+    SELECT
+        hourly.meter_id AS meter_id,
+        SUM(hourly.reading_rate) AS reading
+    FROM meter_hourly_readings_unit hourly
+    WHERE
+        curr_tsrange && hourly.time_interval AND
+        hourly.graphic_unit_id = g_unit_id AND
+        hourly.meter_id = ANY(meter_ids)
+    GROUP BY hourly.meter_id
+	),
+	prev_period AS (
+    SELECT
+        hourly.meter_id AS meter_id,
+        SUM(hourly.reading_rate) AS reading
+    FROM meter_hourly_readings_unit hourly
+    WHERE
+        prev_tsrange && hourly.time_interval AND
+        hourly.graphic_unit_id = g_unit_id AND
+        hourly.meter_id = ANY(meter_ids)
+    GROUP BY hourly.meter_id
+	)
+	SELECT
+		meters.id AS meter_id,
+		curr_period.reading::FLOAT AS curr_use,
+		prev_period.reading::FLOAT AS prev_use
+	FROM
+		unnest(meter_ids) meters(id)
+		-- Left joins here so we get nulls instead of missing rows if readings don't exist for some time intervals
+		LEFT JOIN prev_period ON meters.id = prev_period.meter_id
+		LEFT JOIN curr_period ON meters.id = curr_period.meter_id;
+END;
+$$ LANGUAGE 'plpgsql';
+
+
 /*
 The following function returns data for plotting compare graphs. It works on groups.
 It should not be used on raw readings.
