c2172df6df27384e9831a0d47b2ff8d5daf55135
Talia Frendl <taliafrendl@gmail.com>
2025-07-21 13:44:41 -0700

    added MPL license info to conversionSegment, day, daySegment, and week files

--- src/server/sql/conversionSegment/update_conversion_segment.sql
+++ src/server/sql/conversionSegment/update_conversion_segment.sql
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 -- added functionality for the start_time to be updated, may need to adjust later
 
 UPDATE conversion_segments
