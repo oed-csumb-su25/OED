b9d892f39b3af71fcd1013fa740d5246aa9ae6ba
LeSci-0x1 <sscire@csumb.edu>
2025-07-23 16:11:13 -0700

    update meter_hourly_readings_unit_vB

--- src/server/sql/reading/create_reading_views.sql
+++ src/server/sql/reading/create_reading_views.sql
@@ -426,8 +426,8 @@ CREATE MATERIALIZED VIEW IF NOT EXISTS
 meter_hourly_readings_unit_vB
 	-- vB all rates now include meter conversions from CIK table, changes noted with 'sls_B01'
 	AS SELECT
-		-- This gives the weighted average of the reading rates, defined as
-		-- sum(reading_rate * overlap_duration) / sum(overlap_duration)
+		-- This gives the weighted average of the converted reading rates, defined as
+		-- sum((reading_rate * slope + intercept) * overlap_duration) / sum(overlap_duration)
 		r.meter_id AS meter_id,
 		CASE WHEN u.unit_represent = 'quantity'::unit_represent_type THEN
 			(sum(
@@ -463,7 +463,7 @@ meter_hourly_readings_unit_vB
 			))
 		END AS reading_rate,
 
-		-- The following code does the min/max for hourly readings
+		-- The following code does the converted min/max for hourly readings
 		CASE WHEN u.unit_represent = 'quantity'::unit_represent_type THEN
     		(max(( -- Extract the maximum rate over each day
 				((r.reading * c.slope + c.intercept)  * 3600 / (extract(EPOCH FROM (r.end_timestamp - r.start_timestamp)))) -- Reading rate in kw
@@ -539,13 +539,14 @@ meter_hourly_readings_unit_vB
 	-- This sequence of joins takes the meter id to its unit and a unit.
 	INNER JOIN meters m ON r.meter_id = m.id)
 	INNER JOIN cik c on c.source_id = m.unit_id) -- sls_B01
-	INNER JOIN units u ON c.source_id = u.id)
+	INNER JOIN units u ON c.source_id = u.id) -- sls_B01
 		CROSS JOIN LATERAL generate_series(
 			date_trunc('hour', r.start_timestamp),
 			-- Subtract 1 interval width because generate_series is end-inclusive
 			date_trunc_up('hour', r.end_timestamp) - '1 hour'::INTERVAL,
 			'1 hour'::INTERVAL
 		) gen(interval_start)
+	WHERE tsrange(c.start_time, c.end_time, '()') && tsrange(gen.interval_start, gen.interval_start + INTERVAL '1 hour', '()') -- sls_B03
 	GROUP BY r.meter_id, gen.interval_start, u.unit_represent, graphic_unit_id -- sls_B02
 	-- The order by ensures that the materialized view will be clustered in this way.
 	-- ORDER BY matches current version of meter_hourly_readings_unit -- sls_B01
